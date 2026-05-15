35b4bc08d8b4c2b7000078c7e3183b29d5101c83
Ashish Kumar Tripathi <ashishtring@gmail.com>
2026-05-15 11:58:43 +0530

    Its my project repository
    Signed-off-by: Ashish Kumar Tripathi <ashishtring@gmail.com>

---
+++ SET-MAP.js
@@ -0,0 +1,8 @@
+//set- How to make set using new set ()
+
+var a = [10,20,30,40,50,60,70,80,90,100,10,10,10,10,20,20,20,30,40,40,40,50,50,50,50,60,60,60]
+var b = new Set[a]
+var c = Array.from(b)
+console.log(a)
+console.log(b)
+console.log(c)
